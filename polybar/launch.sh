0cd4edf90b0710b6ebb4dd40e7be10ac713388cf
Sergio Bonatto <52257001+SergioBonatto@users.noreply.github.com>
2022-08-09 23:28:23 -0300

    Add files via upload

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
-DIR="$HOME/.config/polybar"
 # Terminate already running bar instances
 killall -q polybar
+# If all your bars have ipc enabled, you can also use 
+# polybar-msg cmd quit
 
-# Wait until the processes have been shut down
-while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+# Launch bar1 and bar2
+echo "---" | tee -a /tmp/polybar1.log /tmp/polybar2.log
+polybar alg-example 2>&1 | tee -a /tmp/polybar1.log & disown
 
-# Launch the bar
-polybar -q main -c "$DIR"/config.ini &
+echo "Bars launched..."
+
